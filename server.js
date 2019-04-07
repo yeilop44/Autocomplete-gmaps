e1b8357c348d7ee0dc1d311b8f047f3350a048ce
yeilop44 <pc_ylopez@hotmail.com>
2019-04-07 17:45:55 -0500

    commit

--- server.js
+++ server.js
@@ -1,6 +1,17 @@
+    
+//Install express server
 const express = require('express');
+const path = require('path');
+
 const app = express();
 
-app.use(express.static(__dirname + '/dist'));
+// Serve only the static files form the dist directory
+app.use(express.static(__dirname + './dist/autocompletegmaps'));
 
+app.get('/*', function(req,res) {
+    
+res.sendFile(path.join(__dirname+'/dist/autocompletegmaps/index.html'));
+});
+
+// Start the app by listening on the default Heroku port
 app.listen(process.env.PORT || 8080);
